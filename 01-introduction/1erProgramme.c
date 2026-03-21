289ee262586c6c7828dc83e61a669ff0f9501bbf
Skyruu <djaysonancel5@gmail.com>
2026-03-21 12:09:57 +0100

    init: structure complète cours Bro Code + gitignore

---
+++ 01-introduction/1erProgramme.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Bibliothèque standard de C.
+// Toujours mettre des ; 
+
+int main(){  // Fonction principale pour que le code s'execute.
+
+    printf("Mon premier programme C!"); // afficher le texte dans la console
+    printf("\n"); // saut de ligne
+    printf("J'aime la programmation!");
+
+    return 0; // Courrement à la fin du programme. Il indique que le programme à bien fonctionner! 0 = pas d'érreur et valeur non nul = erreur.
+
+} 
