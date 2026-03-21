0d90ee12ce6fcc757181e0a33e391bf9c604964e
Skyruu <djaysonancel5@gmail.com>
2026-03-21 16:34:25 +0100

    2 projets basiques

--- 06-projets-bases/mad_libs.c
+++ 06-projets-bases/mad_libs.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    // MAD LIBS GAME
+
+    // Variable
+    char nom[50] = "";
+    char verbe[50] = "";
+    char adj1[50] = "";
+    char adj2[50] = "";
+    char adj3[50] = "";
+
+    // intéraction avec l'utilisateur
+    printf("Choisis un adjectif (description): ");
+    fgets(adj1, sizeof(adj1), stdin);
+    adj1[strlen(adj1) - 1] = '\0';
+
+    printf("Choisis nom (animal ou personne): ");
+    fgets(nom, sizeof(nom), stdin);
+    nom[strlen(nom) - 1] = '\0';
+
+    printf("Choisis un adjectif (description): ");
+    fgets(adj2, sizeof(adj2), stdin);
+    adj2[strlen(adj2) - 1] = '\0';
+
+    printf("Choisis un verbe (qui se termine par -ant): ");
+    fgets(verbe, sizeof(verbe), stdin);
+    verbe[strlen(verbe) - 1] = '\0';
+
+    printf("Choisis un adjectif (description): ");
+    fgets(adj3, sizeof(adj3), stdin);
+    adj3[strlen(adj3) - 1] = '\0';
+
+// Message
+    printf("\nAujourd'hui je suis allé(e) dans un %s zoo.\n", adj1);
+    printf("Dans une exposition, j'ai vu un(e) %s.\n", nom);
+    printf("%s était %s et %s!\n", nom, adj2, verbe);
+    printf("J'étais %s!\n", adj3);
 
     return 0;
 }
--- 06-projets-bases/shopping_cart.c
+++ 06-projets-bases/shopping_cart.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
+    // SHOPPING CART PROGRAMME
+
+    // Variables
+    char item[50] = "";
+    float prix = 0.0f;
+    int quantité = 0;
+    char monnaie = '$';
+    float total = 0.0f;
+
+    // Intéraction avec l'utilisateur
+    printf("Quel article aimerais-tu acheter?: ");
+    fgets(item, sizeof(item), stdin);
+    item[strlen(item) - 1] = '\0';
+
+    printf("Quel est le prix pour chaque article?: ");
+    scanf("%f", &prix);
+
+    printf("Combien voudrais tu?: ");
+    scanf("%d", &quantité);
+
+    total = prix * quantité;
+
+    printf("\nVous avez acheté(e) %d %s\n", quantité, item);
+    printf("Le total est de: %c%.2f", monnaie, total);
     return 0;
 }
