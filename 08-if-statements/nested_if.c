6097e0d9f6d51259af8b45c689127120d0526f5a
Skyruu <djaysonancel5@gmail.com>
2026-03-22 21:41:04 +0100

    2 leçon en plus!

--- 08-if-statements/nested_if.c
+++ 08-if-statements/nested_if.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
 
+    float prix = 10.00;
+    bool isStudent = true; // 10% de réduction
+    bool isSenior = true; //20% de réduction
+
+    // étudiant = 9€
+    // senior = 8€
+    // étudiant + senior = 7€
+
+    if(isStudent){
+        if(isSenior){ // if imbriqué 
+            printf("Vous bénéficiez d'une réduction étudiante de 10%%\n");
+            printf("Vous bénéficiez d'une réduction sénior de 20%%\n");
+            prix *= 0.7;
+        }
+        else{
+            printf("Vous bénéficiez d'une réduction étudiante de 10%%\n");
+            prix *= 0.9;
+        }
+
+    }
+    else if(isSenior){
+        printf("Vous bénéficiez d'une réduction sénior de 20%%\n");
+        prix *= 0.8;
+    }
+
+    printf("Le prix d'un billet est de: %.2f€\n", prix);
     return 0;
 }
--- 10-switches/switches.c
+++ 10-switches/switches.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 
+// Alternatif de if-else 
+// PLus efficace avec des valeur entier constant
 int main() {
 
+    //int jourDelaSemaine = 0;
+    
+    //printf("Entrez un jour de la semaine (1-7): ");
+    //scanf("%d", &jourDelaSemaine);
+
+        /*switch(jourDelaSemaine){
+        case 1: // équivaut à else if(jourDelaSemaine == 1){}
+        printf("C'est Lundi");
+        break; // Très important!
+    case 2:
+        printf("C'est Mardi");
+        break;
+    case 3:
+        printf("C'est Mercredi");
+        break;
+    case 4:
+        printf("C'est Jeudi");
+        break;
+    case 5:
+        printf("C'est Vendredi");
+        break;
+    case 6:
+        printf("C'est Samedi");
+        break;
+    case 7:
+        printf("C'est Dimanche");
+        break;
+    default:
+        printf("S'il vous plait uniquement un nombre entre 1 et 7"); // Equivaut à else
+    }*/
+
+    char jourDelaSemaine = '\0';
+
+    printf("Entrez un jour de la semaine (L-M-m-J-V-S-D): ");
+    scanf("%c", &jourDelaSemaine);
+
+    switch(jourDelaSemaine){ // uniquement la variable, pas de condition (pas de ==, >, < etc.)
+        case 'L': // C compare jourDelaSemaine à 'L'
+        printf("C'est Lundi");
+        break; // Sans break, le code continue dans les case suivants
+       // Exemple sans break : case 'L' exécuterait aussi Mardi, Mercredi, etc.
+    case 'M':
+        printf("C'est Mardi");
+        break;
+    case 'm':
+        printf("C'est Mercredi");
+        break;
+    case 'J':
+        printf("C'est Jeudi");
+        break;
+    case 'V':
+        printf("C'est Vendredi");
+        break;
+    case 'S':
+        printf("C'est Samedi");
+        break;
+    case 'D':
+        printf("C'est Dimanche");
+        break;
+    default:
+        printf("S'il vous plait uniquement une lettre dans cette liste: (L-M-m-J-V-S-D)"); // Equivaut à else
+    }
     return 0;
 }
